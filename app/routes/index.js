a3ff05bacd3c5fc39b0a8dc689744b33e7d3f5cc
Harish-clb <harish.h2750@gmail.com>
2024-02-17 18:22:14 +0530

    Add request middleware for validating id and update routes for subscriptions

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,11 @@
-const userRoutes = require('./user.routes');
+const userRoutes = require("./user.routes");
+const subscriptionRoutes = require("./subscription.routes");
 
-const BASE_PATH = '/api';
+const BASE_PATH = "/api";
 const USERS_BASE_PATH = `${BASE_PATH}/users`;
+const SUBSCRIPTIONS_BASE_PATH = `${BASE_PATH}/subscriptions`;
 
 module.exports = (app) => {
-    app.use(USERS_BASE_PATH, userRoutes);
+  app.use(USERS_BASE_PATH, userRoutes);
+  app.use(SUBSCRIPTIONS_BASE_PATH, subscriptionRoutes);
 };
